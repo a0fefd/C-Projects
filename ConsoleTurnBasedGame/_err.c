8a3eba878a9e569f2aa90a8a5a4dddec77e96f5b
BIGIC <nbigic@icloud.com>
2024-04-08 22:31:48 +1000

    Update _err.c
    Added class duplicate error

--- ConsoleTurnBasedGame/_err.c
+++ ConsoleTurnBasedGame/_err.c
@@ -3,6 +3,7 @@
 #include "_err.h"
 
 Error _err_ClassNum;
+Error _err_ClassDuplicate;
 
 void _err_init()
 {
@@ -10,14 +11,19 @@ void _err_init()
     strcpy(PTR_err_ClassNum->cause, "Invalid class number.");
     strcpy(PTR_err_ClassNum->message, "Error: A proper class number is required.");
     PTR_err_ClassNum->_return = -1;
+    
+    Error *PTR_err_ClassDuplicate = &_err_ClassDuplicate;
+    strcpy(PTR_err_ClassDuplicate->cause, "Invalid class selection.");
+    strcpy(PTR_err_ClassDuplicate->message, "Error: Each player must select a different class.");
+    PTR_err_ClassDuplicate->_return = -1;
 
-    // free(PTR_err_ClassNum);
 }
 
 Error *getErrors()
 {
     Error *errors = malloc( 8 * sizeof(Error) );
     errors[0] = _err_ClassNum;
+    errors[1] = _err_ClassDuplicate;
 
     return errors;
 }
