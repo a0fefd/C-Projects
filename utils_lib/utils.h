5e8c7b1fef603d30a34e4b87e4dc85e446388224
root <root@WS1759119190.soc.eq.edu.au>
2024-04-09 11:54:33 +1000

    Added bit manipulation

--- utils_lib/utils.h
+++ utils_lib/utils.h
@@ -1,9 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 #ifndef UTILS_H_
 #define UTILS_H_
 
 #ifdef __cplusplus
     extern "C" {
 #endif
+
+#define SET_BIT(byte, bit) ((byte) |= (1 << bit))
+#define CLEAR_BIT(byte, bit) ((byte) &= ~(1 << bit))
+#define TOGGLE_BIT(byte, bit) ((byte) ^= (1 << bit))
+#define CHECK_BIT(byte, bit) ((byte) & (1 << bit))
 
 void clrscr();
 void invert(int *var);
